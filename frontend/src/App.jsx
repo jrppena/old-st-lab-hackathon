d3cfa36bc5f0779aa967cdbbb2d2a456f0afe185
jrppena <jppena@up.edu.ph>
2024-10-17 01:04:30 +0800

    added react-router-dom and tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
+import React from 'react';
 
 function App() {
-
   return (
-    <div>
-      Hello World
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Old St. Labs Hackathon 2024
+      </h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
