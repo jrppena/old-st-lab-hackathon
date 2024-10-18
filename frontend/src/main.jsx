5c720bbdf7915f332142e314a2f323f0fd55faae
jrppena <jppena@up.edu.ph>
2024-10-18 15:16:08 +0800

    added login and signup pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import ListView from './pages/list-view-page.jsx'
 import FaqPage from './pages/faq-page.jsx'
+import Login from './pages/login-page.jsx'
+import Signup from './pages/signup-page.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './assets/css/index.css'; // Adjust path if needed
 
@@ -19,6 +21,14 @@ const router = createBrowserRouter([
     path: '/faq',
     element: <FaqPage />,
   },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/signup',
+    element: <Signup />,
+  }
 ]);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
