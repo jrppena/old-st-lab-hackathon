e7105c1766985ca146636fdd738cf3938ca61212
jrppena <jppena@up.edu.ph>
2024-10-18 20:02:45 +0800

    added user profile page (responsive)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import ListView from './pages/list-view-page.jsx'
 import FaqPage from './pages/faq-page.jsx'
 import Login from './pages/login-page.jsx'
 import Signup from './pages/signup-page.jsx'
+import UserProfile from './pages/user-profile-page.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './assets/css/index.css'; // Adjust path if needed
 
@@ -28,6 +29,10 @@ const router = createBrowserRouter([
   {
     path: '/signup',
     element: <Signup />,
+  },
+  {
+    path: '/user-profile',
+    element: <UserProfile />,
   }
 ]);
 createRoot(document.getElementById('root')).render(
