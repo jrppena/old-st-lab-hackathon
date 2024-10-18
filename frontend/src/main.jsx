63eda2eea30525769ebc366a7a156b0b945ce2f5
FarrelBeso <vabeso@up.edu.ph>
2024-10-18 19:37:54 +0800

    landing page new

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,37 +1,42 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import ListView from './pages/list-view-page.jsx'
-import FaqPage from './pages/faq-page.jsx'
-import Login from './pages/login-page.jsx'
-import Signup from './pages/signup-page.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './assets/css/index.css'; // Adjust path if needed
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import ListView from "./pages/list-view-page.jsx";
+import FaqPage from "./pages/faq-page.jsx";
+import Login from "./pages/login-page.jsx";
+import Signup from "./pages/signup-page.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./assets/css/index.css"; // Adjust path if needed
+import LandingPage from "./pages/landing-page.jsx";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <App />, 
+    path: "/",
+    element: <App />,
   },
   {
-    path: '/list-view',
+    path: "/list-view",
     element: <ListView />,
   },
   {
-    path: '/faq',
+    path: "/faq",
     element: <FaqPage />,
   },
   {
-    path: '/login',
+    path: "/login",
     element: <Login />,
   },
   {
-    path: '/signup',
+    path: "/signup",
     element: <Signup />,
-  }
+  },
+  {
+    path: "/home",
+    element: <LandingPage />,
+  },
 ]);
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
